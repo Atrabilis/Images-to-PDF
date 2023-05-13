48c3340c55e05f9c5b886009c5fc3c4c11dd14a7
Atrabilis <angel.bruno.andrade.osorio@gmail.com>
2023-05-13 03:58:53 -0400

    Avance main

--- main.py
+++ main.py
@@ -6,15 +6,13 @@ from Funciones.quitar_primera_pagina import quitar_primera_pagina
 from Funciones.add_page_numbers import add_page_numbers
 from Funciones.add_page_numbers import add_page_numbers
 from Funciones.create_page_pdf import create_page_pdf
-from Funciones.crear_indice import crear_indice
 from move_last_page_to_second import move_last_page_to_second
 
 clear() 
 pdf = crear_pdf_a_partir_de_imagenes("./Imagenes", "imagenes")
 quitar_primera_pagina("imagenes")
 add_page_numbers("./imagenes.pdf")
-crear_indice("imagenes_numbered.pdf")
-move_last_page_to_second('./final.pdf')
+move_last_page_to_second('./imagenes_numbered.pdf')
 os.remove("./imagenes.pdf")
 
 
