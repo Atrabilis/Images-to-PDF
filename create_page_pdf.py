145f56bce82bbdcde79b17d57b4e907a4a17dff3
Angel Andrade <angel.bruno.andrade.osorio@gmail.com>
2023-05-13 03:40:53 -0400

    Delete create_page_pdf.py

--- create_page_pdf.py
+++
@@ -1,9 +0,0 @@
-from reportlab.pdfgen import canvas
-from reportlab.lib.units import mm
-
-def create_page_pdf(num, tmp):
-    c = canvas.Canvas(tmp)
-    for i in range(1, num + 1):
-        c.drawString((210 // 2) * mm, (4) * mm, str(i))
-        c.showPage()
-    c.save()
